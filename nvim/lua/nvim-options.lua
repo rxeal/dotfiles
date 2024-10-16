e7abbeb2731daa64a7c6bfd1a8adb9ecfdebb6eb
rxeal <rxealdev@gmail.com>
2024-10-16 23:46:32 +0530

    🔧 chore(nvim): configure clipboard and enable line numbers

--- nvim/lua/nvim-options.lua
+++ nvim/lua/nvim-options.lua
@@ -3,3 +3,17 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
+
+vim.wo.number = true
